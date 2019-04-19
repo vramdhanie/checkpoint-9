958a9e70ee1678f5f16a0686592763948355ac2a
vramdhanie <vramdhanie@gmail.com>
2019-04-18 22:36:54 -0400

    Add drill 2

--- index.js
+++ index.js
@@ -21,3 +21,47 @@ function three() {
 }
 
 three();
+
+
+// drill 2
+function start() {
+  console.log('Starting countdown');
+}
+
+function tick(n) {
+  console.log(n);
+}
+
+function done() {
+  console.log('BOOM!');
+}
+
+function countdown(start, tick, done) {
+  start();
+  (function loop(i) {
+    setTimeout(function() {
+      tick(i--);
+      if(i > 0){
+        loop(i);
+      } else {
+        done();
+      }
+    }, 500);
+  })(10);
+}
+
+countdown(start, tick, done);
+
+function startDownload() {
+  console.log('Download starting');
+}
+
+function downloadTick(n) {
+  console.log(`${(n/10)*100}% left`);
+}
+
+function downloadDone() {
+  console.log('Download complete');
+}
+
+countdown(startDownload, downloadTick, downloadDone);
