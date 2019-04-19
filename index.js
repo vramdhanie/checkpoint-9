f69bd34836ad4d3c9d65b5fb55436e8cb8281e3f
vramdhanie <vramdhanie@gmail.com>
2019-04-18 21:52:15 -0400

    Add drill 1

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// drill 1
+function one() {
+  console.log('one');
+}
+
+function two(callback) {
+  console.log('two');
+  callback();
+}
+
+function three() {
+  console.log('three');
+  two(one);
+}
+
+three();
